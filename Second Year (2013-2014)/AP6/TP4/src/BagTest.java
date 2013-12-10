c27d9678143b34eef12cd9061c21cb0f6ddae8cf
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2013-12-10 09:17:41 +0100

    Begin of the tests

--- Second Year (2013-2014)/AP6/TP4/src/BagTest.java
+++ Second Year (2013-2014)/AP6/TP4/src/BagTest.java
@@ -5,11 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import junit.framework.TestCase;
-import javax.swing.JButton;
 
 /**
  * Class test for the Bag Collection
  */
-public class BagTest {
-	
+public class BagTest extends TestCase {
+//Attributes
+	private Bag<Integer> emptyBag;
+	private Bag<Integer> normalBag;
+	private Bag<Integer> testInsertBag;
+	private Bag<String> stringBag;
+
+
+//Method ran before each test case
+	@Before
+	public void setUp() {
+		emptyBag = new Bag<Integer>();
+		normalBag = new Bag<Integer>();
+		testInsertBag = new Bag<Integer>();
+		stringBag = new Bag<String>();
+	}
+
+
+//Methods tests
+//Test of add()
 }
