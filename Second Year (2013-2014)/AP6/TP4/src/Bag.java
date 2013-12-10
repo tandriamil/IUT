5a9bbd4b3aa1b4795710e38456ebc61ef3360487
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2013-12-10 08:25:46 +0100

    Begin of the Bag class

---
+++ Second Year (2013-2014)/AP6/TP4/src/Bag.java
@@ -0,0 +1,23 @@
+package datastruct;
+
+import java.util.AbstractCollection;
+
+/**
+ * Class for the Bag Collection
+ */
+public class Bag<E> {
+//Attributes
+	private Element sentinel;
+	private int size;
+
+
+//Methods
+	/** 
+	 * Constructor of the Bag
+	 */
+	public Bag() {
+		this.size = 0;
+		this.sentinel = new Element(null, null);
+		this.sentinel.next = this.sentinel;
+	}
+}
