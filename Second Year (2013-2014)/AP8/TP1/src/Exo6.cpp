3036b70027bdc320ff950bd83c2a5cbb05cf171e
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-01-15 12:30:22 +0100

    poil

---
+++ Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
@@ -0,0 +1,13 @@
+//Include its Exo5.h
+#include "Exo5.h"
+
+/**
+ * The main method, asking you to enter a chain of characters
+ * and then printing it on the terminal
+ */
+int main() {
+	string s;
+	cout << "entrez votre chaîne de caractères";
+	cin >> s;
+	cout << strlen(s);
+}
