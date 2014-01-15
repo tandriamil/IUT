4221ef5e5456590c2ba00176bfbc865ef47f7812
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-01-15 12:31:05 +0100

    Adding Exo7

--- Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
+++ Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
@@ -1,10 +1,9 @@
-//Include its Exo5.h
-#include "Exo5.h"
+//Include its Exo6.h
+#include "Exo6.h"
 
 /**
  * The main method, here it'll just display "Ca marche!"
  */
 int main() {
-	//Redirect the output to the displays
 	cout << "Coucou! Tu veux ... \n";
 }
