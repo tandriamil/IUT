1b8a7183e485a15b9d1ae342cf0b3b17031d63d3
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-01-15 16:53:48 +0100

    Adding Genre, to finish

---
+++ Second Year (2013-2014)/OMGL6/TP1/src/Genre.java
@@ -0,0 +1,22 @@
+package media;
+
+import java.lang.Enum;
+
+/**
+ * Enum class for the media package
+ */
+public class Genre<E extends Enum<E>> {
+//Attributes
+
+
+//Methods
+	/**
+	 * The constructor
+	 * @param name The name
+	 * @param ordinal ??? 
+	 */
+	public Genre(String name, int ordinal) {
+		
+	}
+
+}
