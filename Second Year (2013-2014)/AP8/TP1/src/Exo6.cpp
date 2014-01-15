a1025f8368e39e5921b30ed4ed46fa353d47ce76
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-01-15 12:55:47 +0100

    Merge

--- Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
+++ Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 //Include its Exo6.h
 #include "Exo6.h"
-
-/**
- * The main method, here it'll just display "Ca marche!"
- */
-int main() {
-	cout << "Coucou! Tu veux ... \n";
-=======
-//Include its Exo5.h
-#include "Exo5.h"
 
 /**
  * The main method, asking you to enter a chain of characters
@@ -20,5 +10,4 @@ int main() {
 	cout << "entrez votre chaîne de caractères";
 	cin >> s;
 	cout << strlen(s);
->>>>>>> 3036b70027bdc320ff950bd83c2a5cbb05cf171e
 }
