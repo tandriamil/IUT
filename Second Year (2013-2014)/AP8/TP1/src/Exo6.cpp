8a94e2aaed740dd9178532bf5c2d4d4db64f40e6
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-01-15 12:53:47 +0100

    ach

--- Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
+++ Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
+
 //Include its Exo6.h
 #include "Exo6.h"
 
-/**
- * The main method, here it'll just display "Ca marche!"
- */
-int main() {
-	cout << "Coucou! Tu veux ... \n";
-=======
-//Include its Exo5.h
-#include "Exo5.h"
 
 /**
  * The main method, asking you to enter a chain of characters
  * and then printing it on the terminal
  */
 int main() {
-<<<<<<< HEAD
+
 	char chain[120];
 	//char stockch[120];
 	char stopchar = '\n';
@@ -30,11 +22,6 @@ int main() {
   	std::cout << strlen (chain);
 
   	return 0;
-=======
-	string s;
-	cout << "entrez votre chaîne de caractères";
-	cin >> s;
-	cout << strlen(s);
->>>>>>> 3036b70027bdc320ff950bd83c2a5cbb05cf171e
->>>>>>> 659d170cabe23b8511448459de80be813db94a18
+
+
 }
