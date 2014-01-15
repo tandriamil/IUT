6f660a158682b242f18584184e355bb732eb8e42
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-01-15 13:02:07 +0100

    ach

--- Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
+++ Second Year (2013-2014)/AP8/TP1/src/Exo6.cpp
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-//Include its Exo6.h
-#include "Exo6.h"
-
-=======
 
 //Include its Exo6.h
 #include "Exo6.h"
 
 
->>>>>>> 8a94e2aaed740dd9178532bf5c2d4d4db64f40e6
 /**
  * The main method, asking you to enter a chain of characters
  * and then printing it on the terminal
  */
 int main() {
-<<<<<<< HEAD
-	string s;
-	cout << "entrez votre chaîne de caractères";
-	cin >> s;
-	cout << strlen(s);
-=======
 
 	char chain[120]; // définit un tableau de caractère de longueur 120 maximum
 	char stopchar = '\n'; // définit le caractère de fin (ici entrée) pour valider la fin de chaîne
@@ -32,6 +20,4 @@ int main() {
 
   	return 0; // code indiquant que tout s'est bien passé
 
-
->>>>>>> 8a94e2aaed740dd9178532bf5c2d4d4db64f40e6
 }
