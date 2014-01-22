9d13a8964491aaae85eb1ba4454f60f9c88c6fa2
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-01-22 11:06:00 +0100

    Editing some code in the AP8 TP2 and Prog2

--- Second Year (2013-2014)/AP8/TP2/Prog2/src/Lanceur2.cpp
+++ Second Year (2013-2014)/AP8/TP2/Prog2/src/Lanceur2.cpp
@@ -1,15 +1,13 @@
 #include "Lanceur2.h"
 
 int main() {
-	
-		CVect2	a(5);
-		fct(a);
-		return 0;
+	CVect2	a(5);
+	fct(a);
+	return 0;
 }
 
 /************************/
 
-void fct ( CVect2 b ) {
-	
-		cout << "Appel de la fonction" << endl;
+void fct (CVect2 b) {
+	cout << "Appel de la fonction" << endl;
 }
