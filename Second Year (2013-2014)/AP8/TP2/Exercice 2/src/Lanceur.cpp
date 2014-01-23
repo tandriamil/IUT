c403bd0a35ed250dbba473295701b7842d029bbe
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-01-23 12:54:58 +0100

    envoi classe test finished !

--- Second Year (2013-2014)/AP8/TP2/Exercice 2/src/Lanceur.cpp
+++ Second Year (2013-2014)/AP8/TP2/Exercice 2/src/Lanceur.cpp
@@ -4,6 +4,8 @@
  * The main method
  */
 int main() {
+
+	// test of constructors and presentation method
 	CPoint c1(4, 8);
 	c1.presentation(); 
 	CPoint c2;
@@ -11,6 +13,8 @@ int main() {
 	CPoint c3(c1);
 	c3.presentation();
 
+
+	// test of modificators 
 	c1.setAbs(5);
 	c1.setOrd(6);
 	c1.presentation();
@@ -22,6 +26,15 @@ int main() {
 	c3.setAbs(12);
 	c3.setOrd(13);
 	c3.presentation();
+
+	// test of accessors
+	cout << "abscisse de c1 : " << c1.getAbs() << endl;
+	cout << "abscisse de c2 : " << c2.getAbs() << endl;
+	cout << "abscisse de c3 : " << c3.getAbs() << endl;
+
+	cout << "ordonnée de c1 : " << c1.getOrd() << endl;
+	cout << "ordonnée de c2 : " << c2.getOrd() << endl;
+	cout << "ordonnée de c3 : " << c3.getOrd() << endl;
 
 	return 0;
 }
