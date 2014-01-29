8e5459b3e7157ea4764b6ff3a99b35a6fb34752b
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-01-29 12:51:13 +0100

    ach

--- Second Year (2013-2014)/AP8/TP3/Exercice 2/src/CManFch.h
+++ Second Year (2013-2014)/AP8/TP3/Exercice 2/src/CManFch.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
--- Second Year (2013-2014)/AP8/TP3/Exercice 2/src/CManFich.cpp
+++ Second Year (2013-2014)/AP8/TP3/Exercice 2/src/CManFich.cpp
@@ -16,6 +16,29 @@ CManFch::CManFch() {
  * @param str The name of the file with it location
  */
 CManFch::CManFch(string str) {
+	
+	size_t found=str.find_last_of("/\\"); // cherche le dernier "/"
+
+  	this->m_NomFich = str.substr(found+1); // prend tout ce qu'il y a après le dernier "/"
+	this->m_EmplcmtFich = str.substr(0,found); // prend tout ce qu'il y a avant le dernier "/"
+	
+	found = this -> m_NomFich.find_last_of("."); // cherche le "."
+	string fileExtension = this -> m_NomFich.substr(found+1); // prend tout ce qu'il y après le "."
+	
+	char tabChar[6]="txt"; // création d'un tableau statique contenant "txt"
+	char tabChar2[6]; // création d'un tableau statique contenant l'extension trouvée du fichier
+
+	for (int i=0; i < 6; i++) {
+		tabChar2[i] = fileExtension[i]; // recopie de fileExtention dans tabChar2
+	}
+
+	int test = strcmp(tabChar,tabChar2); // comparaison des 2 chaîness
+	if (test == 0 ){
+		this -> m_TypeFich = 1;
+	}
+
+
+	cout << "nom du fichier : " << this -> m_NomFich << "\n" << "emplacement du fichier : " << this -> m_EmplcmtFich << "\n" << "extension du fichier : "<< this -> m_TypeFich <<endl;
 
 }
 
@@ -40,7 +63,7 @@ void CManFch::setNomFichier(string str) {
  * Displays the attributes
  */
 void CManFch::afficherAttributs() {
-	cout << "Nom du fichier: " << this->m_NomFich << "\n" << "Emplacement du fichier: " << this->m_EmplcmtFich << "\n" << "Type du fichier: " << this->m_TypeFich << "\n";
+	cout << "Nom du fichier : " << this-> m_NomFich << "\n" << "Emplacement du fichier: " << this->m_EmplcmtFich << "\n" << "Type du fichier: " << this->m_TypeFich << "\n";
 }
 
 
@@ -54,7 +77,7 @@ void CManFch::afficherFichier() {
 	}
 
 	//If it's a .bin file
-	if ($this->m_TypeFich == 2) {
+	if (this->m_TypeFich == 2) {
 
 	}
 
