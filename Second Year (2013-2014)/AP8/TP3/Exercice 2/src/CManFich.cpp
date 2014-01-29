f0b12885ff959259ac042aa9fb89f40fea17afbc
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-01-29 11:18:08 +0100

    Beginning of the second exercice

---
+++ Second Year (2013-2014)/AP8/TP3/Exercice 2/src/CManFich.cpp
@@ -0,0 +1,16 @@
+#include "CManFch.h"
+
+/**
+ * The first constructor with no parameters
+ */
+CManFch::CManFch() {
+	//Initializes the attributes
+	this->m_NomFich = "";
+	this->m_EmplcmtFich = "";
+	this->m_TypeFich = 0;
+}
+
+
+/** 
+ * The second constructor with 
+ */
