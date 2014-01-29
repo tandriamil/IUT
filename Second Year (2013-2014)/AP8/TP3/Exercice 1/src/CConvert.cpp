c2c6ce014e08182ac1fd1ff30eb002b2a0ef190f
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-01-29 10:19:26 +0100

    CConvert

--- Second Year (2013-2014)/AP8/TP3/Exercice 1/src/CConvert.cpp
+++ Second Year (2013-2014)/AP8/TP3/Exercice 1/src/CConvert.cpp
@@ -11,3 +11,39 @@ CConvert::CConvert() {
 	//Then prints a message
 	cout << "Construction et initialisation des variables à 0 et \"\" de l'objet CPoint d'adresse : " << this << endl;
 }
+
+
+/**
+ * The constructor with only a string
+ * @param str The string to initialize the attribute
+ */
+CConvert::CConvert(string str) {
+	//Initializes the two attributes
+	this->m_Str = str;
+	this->m_Dbl = 0;
+
+	//Then prints a message
+	cout << "Construction et initialisation des variables à 0 et à " << str << " de l'objet CPoint d'adresse : " << this << endl;
+}
+
+
+/**
+ * The last constructor with only a double
+ * @param dbl The double to initialize the attribute
+ */
+CConvert::CConvert(double dbl) {
+ 	//Initializes the two attributes
+	this->m_Str = "";
+	this->m_Dbl = dbl;
+
+	//Then prints a message
+	cout << "Construction et initialisation des variables à une valeur et à \"\" de l'objet CPoint d'adresse : " << this << endl;
+}
+
+
+/**
+ * The destructor
+ */
+CConvert::~CConvert() {
+	cout << "Destruction de l'objet CConvert d'adresse : " << this << "\n" << endl;
+}
