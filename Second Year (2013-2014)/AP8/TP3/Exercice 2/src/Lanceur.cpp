b2e9fbdf3c245a1372e11c9344dbc697809865ea
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-02-03 23:46:46 +0100

    ach

--- Second Year (2013-2014)/AP8/TP3/Exercice 2/src/Lanceur.cpp
+++ Second Year (2013-2014)/AP8/TP3/Exercice 2/src/Lanceur.cpp
@@ -2,16 +2,16 @@
 
 int main () {
 
-	CManFch f1 ("/home/nap/Téléchargements/coin.txt");
+	CManFch f1 ("/home/galvorn/Téléchargements/coin.txt");
 	f1.afficherAttributs();
 	f1.afficherFichier();
 
-	CManFch f2 ("/home/nap/Téléchargements/coin.ach");
+	CManFch f2 ("/home/galvorn/Téléchargements/coin.ach");
 	f2.afficherAttributs();
 	f2.afficherFichier();
 
-	CManFch f3 ("/home/nap/Téléchargements/coin.bin");
+	/*CManFch f3 ("/home/nap/Téléchargements/coin.bin");
 	f3.afficherAttributs();
-	f3.afficherFichier();
+	f3.afficherFichier();*/
 
 }
