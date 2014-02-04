d3e8ee02a5db77367e6110550b56f02185a75426
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-04 12:34:16 +0100

    Updating the nap2 application, server works

--- Second Year (2013-2014)/ASR6/nap2/src/Serveur.java
+++ Second Year (2013-2014)/ASR6/nap2/src/Serveur.java
@@ -12,30 +12,32 @@ import java.io.OutputStreamWriter;
  * The server class
  */
 public class Serveur {
+//Attributes
+    private ServerSocket socketserver = null;
+    private Socket socketduserveur = null;
+    private int numPort;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+    private boolean waitNext;
+    private Client[] listClient;
 
+
+//Methods
     /** 
-     * The main method to be thrown
+     * The constructor
+     * @param port The number of the port to use
      */
-	public static void main(String[] args) {
-        
-        //Creates the sockets
-        ServerSocket socketserver = null;
-        Socket socketduserveur = null;
+    public Serveur(int port) {
+        //Initializes the port, the boolean and the list of client
+        this.numPort = port;
+        this.listClient = new Client[10];
+        this.waitNext = true;
 
-        //Gets the port
-        int numPort = Integer.parseInt(args[0]);
-
-        //Creates the buffer and the writer to send/receive messages
-        BufferedReader in = null;
-        PrintWriter out = null;
-
-        //The boolean to continue
-        boolean waitNext = true;
 
 
         //Creates the ServerSocket and catches errors if there are some
         try {
-            socketserver = new ServerSocket(numPort);
+            this.socketserver = new ServerSocket(this.numPort);
         }
 
         catch (IOException e) {
@@ -48,13 +50,13 @@ public class Serveur {
             e.printStackTrace();
         }
 
-        System.out.println("Le ServerSocket est correctement créer sur le port " + numPort + ".");
+        System.out.println("ServerSocket correctly created on the port number " + this.numPort + ".");
 
 
 
         //Creates the Socket of the server and catches errors if there are some
         try {
-            socketduserveur = new Socket(InetAddress.getLocalHost(), numPort);
+            this.socketduserveur = new Socket(InetAddress.getLocalHost(), this.numPort);
         }
 
         catch (IOException e) {
@@ -67,19 +69,7 @@ public class Serveur {
             e.printStackTrace();
         }
 
-        System.out.println("Le socketduserveur est correctement créer en localhost sur le port " + numPort + ".");
-
-
-
-        //Accepts sockets incomings
-        try {
-            socketduserveur = socketserver.accept();
-        }
-
-        catch (IOException e) {
-            System.out.println("IOException catched when the socketduserveur waits some sockets: " + "\n" + e.getMessage());
-            e.printStackTrace();
-        }
+        System.out.println("Server's socket created on localhost:" + this.numPort + ".");
 
 
 
@@ -92,6 +82,8 @@ public class Serveur {
             System.out.println("IOException catched when initializing the BufferedReader: " + "\n" + e.getMessage());
             e.printStackTrace();
         }
+
+        System.out.println ("BufferedReader correctly created!");
 
 
 
@@ -106,14 +98,75 @@ public class Serveur {
             e.printStackTrace();
         }
 
-
-        //If everything's ok until there
-        System.out.println ("Serveur lancé!");
+        System.out.println ("PrintWriter correctly created!");
 
 
 
+        //Accepts sockets incomings
+        try {
+            socketduserveur = socketserver.accept();
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException catched when the socketduserveur waits some sockets: " + "\n" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println ("The server accepts sockets now.");
+    }
+
+
+    /**
+     * Permits to close the server
+     */
+    public void closeServer() {
+        //Closes the reader/writer
+        try {
+            in.close();
+            out.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException catched when closing in and out: " + "\n" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("Reader/Writer correctly closed.");
+
+
+
+        //Then finish by closing the two sockets
+        try {
+            this.socketserver.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException catched when closing the ServerSocket: " + "\n" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("ServerSocket is correctly closed.");
+
+
+        try {
+            this.socketduserveur.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException catched when closing the socketduserveur: " + "\n" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("Server's socket is correctly closed.");
+    }
+
+
+    /**
+     * Waits a connection and messages
+     */
+    public void waitConnection() {
         //Then the boucle to wait
-        while (waitNext) {
+        while (this.waitNext) {
             String str = "";
 
             //Read the message
@@ -134,32 +187,5 @@ public class Serveur {
             System.out.println("Message: " + str);   // trace locale
             out.println(str);
         }
-
-
-
-        //Then finish by closing the two sockets
-        try {
-            socketserver.close();
-        }
-
-        catch (IOException e) {
-            System.out.println("IOException catched when closing the ServerSocket: " + "\n" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        System.out.println("Le ServerSocket est correctement fermé.");
-
-
-        try {
-            socketduserveur.close();
-        }
-
-        catch (IOException e) {
-            System.out.println("IOException catched when closing the socketduserveur: " + "\n" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        System.out.println("Le socketduserveur est correctement fermé.");
     }
-
 }
