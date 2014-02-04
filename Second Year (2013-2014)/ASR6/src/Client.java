375d8d1b7fd62e299629a43307572af9b0044274
e1200806 <duclos.e1200806@etud.univ-ubs.fr>
2014-02-04 10:47:46 +0100

    ach

--- Second Year (2013-2014)/ASR6/src/Client.java
+++ Second Year (2013-2014)/ASR6/src/Client.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -7,17 +10,38 @@ import java.net.UnknownHostException;
 public class Client {
     
     public static void main(String[] zero) {
-        
-        Socket socket;
+
+
+        Socket socket; // socket entre le client et le serveur
+        BufferedReader in; // buffer in pour récupérer des messages
+        PrintWriter out; // printer out pour envoyer des messages
+
         try {
-             socket = new Socket(InetAddress.getLocalHost(),80); 
-             System.out.println ("connexion au serveur réussie !");
-             socket.close();
+            
+            // demande de connexion sur le port 1234
+            socket = new Socket(InetAddress.getLocalHost(),1234);   
+            System.out.println("Demande de connexion");
+
+            // création d'un printer-out pour transmettre un message au server
+            out = new PrintWriter(socket.getOutputStream());
+            out.println("message du client : connexion établie !");
+            out.flush();
+
+            // création du lecteur pour récupérer un éventuel message venant du server
+            in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+            String message_distant = in.readLine();
+            System.out.println(message_distant);
+            
+            // fermeture du socket     
+            socket.close();
+               
         }
-        catch (UnknownHostException e) {   
+
+        catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        catch (IOException e) {    
+
+        catch (IOException e) {   
             e.printStackTrace();
         }
     }
--- Second Year (2013-2014)/ASR6/src/Serveur.java
+++ Second Year (2013-2014)/ASR6/src/Serveur.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.PrintWriter;
 
 public class Serveur {
 
@@ -8,17 +14,34 @@ public class Serveur {
         
         ServerSocket socketserver;
         Socket socketduserveur;
+        BufferedReader in; // buffer in pour récupérer des messages
+        PrintWriter out; // printer-out pour envoyer des messages
 
         try {
-            // création d'un objet serveur à l'écoute du port 80
-            socketserver = new ServerSocket(80);
-            System.out.println ("socketserver lancé !");
-
+        
+            // création d'un servercosket à l'écoute sur le port 1234
+            socketserver = new ServerSocket(1234);
+            System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
+            
+            // attente d'une demande de connexion provenant du client
             socketduserveur = socketserver.accept(); 
-            System.out.println("connexion réussie et établissement d'un socket entre le serveur et le client !");
-            socketserver.close();
+
+            // création d'un printer-out pour transmettre un message au client
+            out = new PrintWriter(socketduserveur.getOutputStream());
+            out.println("message du serveur : connexion établie !");
+            out.flush();
+
+            // création du lecteur pour récupérer un éventuel message venant du client
+            in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream()));
+            String message_distant = in.readLine();
+            System.out.println(message_distant);
+                       
+            // fermeture du socket puis du serversocket           
             socketduserveur.close();
+            socketserver.close();
+                
         }
+
         catch (IOException e) {
             e.printStackTrace();
         }
