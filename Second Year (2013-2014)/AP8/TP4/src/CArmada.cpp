9b7ef74c00ca4c165a9f098bf991da7b1987fb92
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-05 10:41:48 +0100

    -am

--- Second Year (2013-2014)/AP8/TP4/src/CArmada.cpp
+++ Second Year (2013-2014)/AP8/TP4/src/CArmada.cpp
@@ -18,3 +18,20 @@ CArmada : ~CArmada () {
 	cout << "destruction de l'objet CArmada d'adresse : " << this << "et libération de l'espace mémoire du tableau " << "\n" << endl;
 	delete [] m_pTabSousMarins;
 }
+
+
+/**
+ * Analyses a string to know if it's correct
+ * @param saisie The string ot analyse
+ */
+CArmada :: analyser(string saisie) {
+	//If the string does'nt have exactrly 2 chars
+	if (!(saisie.size() == 2)) {
+		throw logic_error("Taille du string entré incorrect.");
+	}
+
+	//If it has exactly 2 chars
+	else {
+		
+	}
+}
