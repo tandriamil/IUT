d24dc5891c987a557bdef3126bf21ce19af30793
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-05 10:04:27 +0100

    Adding the Biblio.h

---
+++ Second Year (2013-2014)/AP8/TP4/src/BiblioStd.h
@@ -0,0 +1,15 @@
+#ifndef BiblioStd_H_
+#define BiblioStd_H_
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+#endif
