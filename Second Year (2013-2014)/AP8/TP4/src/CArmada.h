419d07f9472f9e9955e7589367da1d1de5b261a9
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-05 10:23:13 +0100

    Adding CArmada.h

---
+++ Second Year (2013-2014)/AP8/TP4/src/CArmada.h
@@ -0,0 +1,41 @@
+#ifndef CArmada_H_
+#define CArmada_H_
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+#include <cstdlib>
+
+using namespace std;
+
+class CArmada {
+//Attributes
+	private:
+		//The table for all the submarines
+		string* m_pTabSousMarins;
+
+		//The total number of submarines
+		int m_nbTotSSM;
+
+
+//Methods
+	public:
+		//Constructors, destructors
+		CArmada(int nbsub);
+		~CArmada();
+
+		//Accessors
+		string* getTabSM();
+		int getNbSub();
+
+		//Other methods
+		void remplirStruct();
+
+
+	private:
+		void analyser(string saisie);
+};
+
+#endif
