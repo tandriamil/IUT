35e73e11de8ec1e248b53a0a2c62f1fe5955c140
Yoann <yoann@yoann-K52JT.(none)>
2014-02-12 13:01:27 +0100

    Noir

--- Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/COPiece.cpp
+++ Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/COPiece.cpp
@@ -1,11 +1,3 @@
-//========================================================================
-// Projet C/C++
-//
-//------------------------------------------------------------------------
-// Copyright (c) 2013-2014 Equipe SEASIDE (T. LE NAOUR, J.F KAMP)
-// IRISA - UBS  / IUT Informatique Vannes
-//========================================================================
-
 #include "COPiece.h"
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -14,3 +6,38 @@
 //
 //-------------------------------------------------------------------------------------------------------------------
 
+// Constructor of the COPiece class
+COPiece::COPiece(unsigned int dim, int iX, int iY) {
+	// Creates the color of the O piece
+	CVector3 col = new CVector3(1.0, 1.0, 0.0)
+
+	// We call the attributes of the super-class
+	super(dim, iX, iY, &col);
+
+	cout << "COPiece created in case " << this << endl;
+}
+
+/********************************************************************************************************/
+
+/**
+* The destructor
+*/
+COPiece::~COPiece() {
+
+	cout << "COPiece in case " << this << " deleted " << endl;
+}
+
+/********************************************************************************************************/
+
+/**
+* The method turn which allows to rotate the piece in terms of its state
+*/
+void COPiece::Turn() {
+	// 1st case : where the O piece is turned once
+	if ((this -> m_state == S_Top) || (this -> m_state == S_Bottom)) {
+		
+	}
+	
+}
+
+/********************************************************************************************************/
