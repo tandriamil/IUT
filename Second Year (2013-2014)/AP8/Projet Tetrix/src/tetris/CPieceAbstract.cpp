c99d8accd74db9a8650da9d028538dae3770f1de
galvorn <galvorn@localhost.localdomain>
2014-02-12 12:18:33 +0100

    les 2 turn()

--- Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/CPieceAbstract.cpp
+++ Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/CPieceAbstract.cpp
@@ -31,58 +31,133 @@ CPieceAbstract::~CPieceAbstract() {
 }
 
 /****************************************/
+/* accesseur m_table */
 
 TPieceTable& CPieceAbstract::GetTable() {
 	return this->m_table;
 }
 
 /****************************************/
+/* modificateur m_table */
 
 void CPieceAbstract::SetTable(const TPieceTable& table) {
 	this->m_table = table;
 }
 
 /****************************************/
+/* accesseur m_colIndex */
 
 int CPieceAbstract::GetColIndex() {
 	return this->m_colIndex;
 }
 
 /****************************************/
+/* accesseur m_rowIndex */
 
 int CPieceAbstract::GetRowIndex() {
 	return this->m_rowIndex;
 }
 
 /****************************************/
+/* modificateur m_rowIndex */
 
 void CPieceAbstract::SetIncDecRowIndex(int val) {
 	this->m_rowIndex = this->m_rowIndex + val;
 }
 
 /****************************************/
+/* modificateur ajouteur m_colIndex */
 
 void CPieceAbstract::SetIncDecColIndex(int val) {
 	this->m_colIndex = this->m_colIndex + val;
 }
 
 /****************************************/
+/* modificateur m_colIndex */
 
 void CPieceAbstract::SetColIndex(int newColIndex) {
 	this->m_colIndex = newColIndex;
 }
 
 /****************************************/
+/* modificateur m_rowIndex */
 
 void CPieceAbstract::SetRowIndex(int newRowIndex) {
 	this->m_rowIndex = newRowIndex;
 }
 
 /****************************************/
+/* accesseur m_color */
 
 const CVector3& CPieceAbstract::GetColor() {
 	return this->m_color;
 }
+
+/****************************************/
+/* turn right method */
+
+void CPieceAbstract::TurnRight() {
+
+	switch (m_state)
+	{
+		case S_Top:
+		{
+			this -> m_state = S_Right;
+			break;
+		}
+
+		case S_Bottom:
+		{
+			this -> m_state = S_Left;
+			break;
+		}
+
+		case S_Left:
+		{
+			this -> m_state = S_Top;
+			break;
+		} 
+
+		case S_Right:
+		{
+			this -> m_state = S_Bottom;
+		}
+	}
+	
+}
+
+/**************************************/
+/* turn left method */
+
+void CPieceAbstract::TurnLeft() {
+
+	switch (m_state)
+	{
+		case S_Top:
+		{
+			this -> m_state = S_Left;
+			break;
+		}
+
+		case S_Bottom:
+		{
+			this -> m_state = S_Right;
+			break;
+		}
+
+		case S_Left:
+		{
+			this -> m_state = S_Bottom;
+			break;
+		} 
+
+		case S_Right:
+		{
+			this -> m_state = S_Top;
+		}
+	}
+}		
+
 
 /****************************************/
 
