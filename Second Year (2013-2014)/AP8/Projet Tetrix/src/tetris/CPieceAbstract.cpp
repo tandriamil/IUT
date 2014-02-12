4da3c45e8422e72f507f1f8e0168867883284241
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-12 10:50:05 +0100

    Updating CPieceAbstract

--- Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/CPieceAbstract.cpp
+++ Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/CPieceAbstract.cpp
@@ -15,7 +15,6 @@
 //-------------------------------------------------------------------------------------------------------------------
 
 CPieceAbstract::CPieceAbstract ( unsigned int dim, int iX, int iY, const CVector3& color ) {
-
 	m_dim = dim;
 	TPieceRow rows = TPieceRow ( dim, 0 );
 	m_table = TPieceTable ( dim, rows );
@@ -25,6 +24,69 @@ CPieceAbstract::CPieceAbstract ( unsigned int dim, int iX, int iY, const CVector
 	m_color = color;
 }
 
-CPieceAbstract::~CPieceAbstract()
-{
+/****************************************/
+
+CPieceAbstract::~CPieceAbstract() {
+	cout << "Piece correctement detruite." << endl;
 }
+
+/****************************************/
+
+TPieceTable& CPieceAbstract::GetTable() {
+	return this->m_table;
+}
+
+/****************************************/
+
+void CPieceAbstract::SetTable(const TPieceTable& table) {
+	this->m_table = table;
+}
+
+/****************************************/
+
+int CPieceAbstract::GetColIndex() {
+	return this->m_colIndex;
+}
+
+/****************************************/
+
+int CPieceAbstract::GetRowIndex() {
+	return this->m_rowIndex;
+}
+
+/****************************************/
+
+void CPieceAbstract::SetIncDecRowIndex(int val) {
+	this->m_rowIndex = this->m_rowIndex + val;
+}
+
+/****************************************/
+
+void CPieceAbstract::SetIncDecColIndex(int val) {
+	this->m_colIndex = this->m_colIndex + val;
+}
+
+/****************************************/
+
+void CPieceAbstract::SetColIndex(int newColIndex) {
+	this->m_colIndex = newColIndex;
+}
+
+/****************************************/
+
+void CPieceAbstract::SetRowIndex(int newRowIndex) {
+	this->m_rowIndex = newRowIndex;
+}
+
+/****************************************/
+
+const CVector3& CPieceAbstract::GetColor() {
+	return this->m_color;
+}
+
+/****************************************/
+
+/*friend iostream& CPieceAbstract::operator<< (iostream& flux, const CPieceAbstract& p) {
+	
+}
+*/
