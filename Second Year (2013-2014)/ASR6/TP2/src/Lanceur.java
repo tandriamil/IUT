0f1867934dd793b46805ba42c953656e96c236e5
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-12 08:41:07 +0100

    Adding the TP2 beginning

--- Second Year (2013-2014)/ASR6/TP2/src/Lanceur.java
+++ Second Year (2013-2014)/ASR6/TP2/src/Lanceur.java
@@ -1,8 +1,24 @@
 /**
- * The main class
+ * The class to launch the application
  */
 public class Lanceur {
-	public static void main (String[] args) {
-		
+
+	/**
+	 * The main method to run the application
+	 */
+	public static void main(String[] args) {
+		//Gets the port
+		int port = Integer.parseInt(args[0]);
+		System.out.println("Lancement de l'application sur le port " + port + "." + "\n" + "################################################");
+
+		//Creates the server
+		Serveur serv = new Serveur(port);
+
+		//Then waits some connection
+		serv.waitConnection();
+
+		//When finished, close it
+		serv.closeServer();
 	}
+
 }
