d0ffb7a166311a6a64e8cbccee98f9810525650a
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-13 12:43:34 +0100

    Continuing the test classes for pieces

--- Second Year (2013-2014)/AP8/Projet Tetrix/src/testCPiece/testCIPiece.cpp
+++ Second Year (2013-2014)/AP8/Projet Tetrix/src/testCPiece/testCIPiece.cpp
@@ -1,8 +1,30 @@
-// Test class of CIPiece
+//Test class of CIPiece
 
-class testCIPiece {
-public:
-	testCIPiece();
-	~testCIPiece();
-	
-};
+#include "testCIPiece.h"
+
+/**
+	\brief Main method to test the CIPiece class
+ */
+int main {
+//Initialisation
+	//Creates the color vector
+	CVector3 color = CVector3(0.0f, 1.0f, 0.0f);
+
+	//Creates some objects CIPiece
+	CIPiece c1 = new CIPiece(3, 500, 500, &color);
+	CIPiece c2 = new CIPiece(3, 400, 400, &color);
+	CIPiece c3 = new CIPiece(3, 300, 300, &color);
+
+	//
+
+	cout << "The initialisation succeed." << endl;
+
+
+//Test of methods
+	//Test of GetColIndex()
+	cout << "Test of GetColIndex():" << endl;
+	cout << "The colIndex of c1 " << c1.GetColIndex() << endl;
+	cout << "The colIndex of c2 " << c2.GetColIndex() << endl;
+	cout << "The colIndex of c3 " << c3.GetColIndex() << endl;
+
+}
