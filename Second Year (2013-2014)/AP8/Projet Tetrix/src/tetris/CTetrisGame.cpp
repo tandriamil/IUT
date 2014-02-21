1afd9ca649dd783176d4cdc18a8c956d5ef3b1f7
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-02-21 22:35:38 +0100

    Updating InsertPiece method

--- Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/CTetrisGame.cpp
+++ Second Year (2013-2014)/AP8/Projet Tetrix/src/tetris/CTetrisGame.cpp
@@ -199,6 +199,27 @@ void CTetrisGame::InsertPiece() {
 	//Getting the rows
 	vector<TGameRow> rows = this->m_board.GetGameTable();
 
+	//Here, will color all the cases of the piece
+	//-1 because the center of the case is considered as the case [1][1] and we'll color from [0][0] 
+	for (int i = colId - 1; i < 3; i++) {
+		for (int j = rowId - 1; j < 3; j++) {
+			//If it's in the case table
+			if (table[colId][rowId] == 1) {
+				//Then gets the row of the piece
+				vector<Case> pieceRow = rows[rowId];
+
+				//Then gets the case from the row of the piece by its col
+				Case c = pieceRow[colId];
+
+				//Then colors it to the color of the piece and make it used
+				c.m_color = color;
+				c.m_used = true;
+			}
+		}
+	}
+
+
+	/*
 	//Then gets the row of the piece
 	vector<Case> pieceRow = rows[rowId];
 
@@ -208,6 +229,7 @@ void CTetrisGame::InsertPiece() {
 	//Then colors it to the color of the piece and make it used
 	c.m_color = color;
 	c.m_used = true;
+	*/
 }
 
 /****************************************/
